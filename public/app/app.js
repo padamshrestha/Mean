68df26c74e23d0ccbdff8bc8eee2aea7f4926628
padamshrestha <padamshres@gmail.com>
2014-03-13 23:29:56 -0700

    Installed dependencies, crated node, added stylus, created layout and created angular app

--- public/app/app.js
+++ public/app/app.js
@@ -1,3 +1,15 @@
 /**
  * Created by padam on 3/13/14.
  */
+
+angular.module('app', ['ngResource', 'ngRoute']);
+
+angular.module('app').config(function($routeProvider, $locationProvider){
+    $locationProvider.html5Mode(true);
+    $routeProvider
+        .when('/', { templateUrl: '/partials/main', controller:'mainCtrl'});
+});
+
+angular.module('app').controller('mainCtrl', function($scope){
+    $scope.myVar = "Hello Angular";
+});
